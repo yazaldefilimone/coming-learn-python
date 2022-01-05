a70aa95bbb3ed4949adf5594b1c875ef80e981d1
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-05 19:44:46 +0200

    estudando sobre listas

---
+++ src/listas-em-python.py
@@ -0,0 +1,21 @@
+istoEUmaLista = [];
+
+print(type(istoEUmaLista));#isto vai retornar o tipo list
+
+#podemos adicionar valores na lista.
+
+istoEUmaListaDeNumerosInteiros = [1, 7, 3, 8, 9, 0]; #uma lista de numeros inteiros
+
+
+print(istoEUmaListaDeNumerosInteiros);
+
+#Numa lista podemos armazenar elementos de qualquer tipo.
+
+umaListaQualquer = ['ola', 17, 1.7888, True, []];
+
+
+print(len(umaListaQualquer));# isto retorna a quantidade de intem da lista
+
+
+#para acessar algum elemento da lista.
+rint(umaListaQualquer[0]); # ola porque o python inicia a contagem do 0 nao do 1.
