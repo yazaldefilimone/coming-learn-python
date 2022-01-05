fee20d9ae637fb26a95e1100f10a4d01b8fc6676
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-05 18:43:38 +0200

    usando strings em python

---
+++ src/strings.py
@@ -0,0 +1,9 @@
+# basucamente uma string em uma cadeia de caracteres.
+ 
+
+
+print('ola eu sou uma string.');
+
+# e o tipo do mesmo e str, que e abriviatura de string.
+
+type('eu sou uma string');
