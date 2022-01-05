d8138b5907942876b4de8a2af21df66dbdaafc9e
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-05 19:33:12 +0200

    conhecendo as funcoes

---
+++ src/funcoes.py
@@ -0,0 +1,17 @@
+# uma funcao pode ser feita pra armazebar codigo reutilizaveil.
+
+#uma funcao pode receber quantos parametrod forem.
+def verificaAIdadeDoUsuario(idade3, nome):
+  #normalmente uma funcao sempre retirna alguma coisa mas isso nao te impede  de mudar o return pelo print
+    if idade3 < 18:
+        return f'voce se chama { nome } e é menor de idade { idade3 }'
+
+    elif idade3 >= 60:
+        return f'voce se chama { nome } e é idosa: { idade3 }'
+
+    else:
+        return f'voce se chama { nome  } e é maior de idade { idade3 }'
+
+
+print(verificaAIdadeDoUsuario(18, 'yazalde'));
+
