410b99f134977bf7fbe81e326b580992f471d296
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-05 19:01:30 +0200

    aprendendo a quebrar linhas nas strings

--- src/strings.py
+++ src/strings.py
@@ -3,6 +3,12 @@
 
 
 print('ola eu sou uma string.');
+print("ola eu sou uma string.");
+print('''ola eu sou uma string.'''); # este tipo de string te perminfe quebrar a linha.
+
+print("""ola
+      eu sou uma string.""");
+print("ola, \n eu sou uma string."); # este e a forma de quebrar a linha tradicional
 
 # e o tipo do mesmo e str, que e abriviatura de string.
 
