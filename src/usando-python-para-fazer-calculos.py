152261c218a2957597c04bbc95ad22bce5e5b6ad
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-05 18:43:17 +0200

    usando python para fazer calculos

---
+++ src/usando-python-para-fazer-calculos.py
@@ -0,0 +1,22 @@
+# Adicao.
+print(2 + 3); #5
+
+#multiplicacao
+print(2 * 3) #6
+
+# subtração
+print(2 - 3) #1
+
+# divisao
+print(2 / 3) # isso retorna um numero ponto flutuante. 0.666666666
+
+#para retornar um valor inteiro
+
+print(2 // 3) #  0
+
+# pontecia
+
+print(2 ** 3) # 
+
+
+
