a772b33843c3829e052d3bc6b210bad26bb61c14
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-05 18:57:13 +0200

    estudando a manipulacao de valores logicos

--- src/boolean.py
+++ src/boolean.py
@@ -3,5 +3,15 @@
 print(True);
 print(False);
 
+
+# retorna  True(verdadeiro) se todos os valores forem veradeiros-(True)
+print(True and True)
+print(True and False)
+print(False and False)
+
+# retorna  True(verdadeiro) se um dos valores for veradeiro(True)
+print(True or True)
+print(True or False)
+print(False or False)
 # pode percever que os primiros caracteres sao e masculino.
 
