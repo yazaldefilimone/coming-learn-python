4f4fc5a8f9346611cae90d118788939b1d26d6d9
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-05 18:51:02 +0200

    estudando sobre variaveis em python

---
+++ src/variaveis.py
@@ -0,0 +1,13 @@
+# estas sao as formas mas communs de definir variaveis em python.
+numeroInteiro = 125;
+numero_inteiro = 256;
+numero_2 = 2;
+numero = 327;
+
+
+print(numeroInteiro);
+print(numero_inteiro);
+print(numero_2);
+print(numero);
+
+
