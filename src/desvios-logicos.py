e8af24160c70cee3f960d1b3bb0f87a815eb9389
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-05 19:17:04 +0200

    aprendendo sobre desvio de valores logicos o if 3else

--- src/desvios-logicos.py
+++ src/desvios-logicos.py
@@ -3,3 +3,13 @@ idade = 17;
 # isso so recebe valores boolean so vai imprimir se o valor for verdadeiro(True)
 if idade < 18:
     print(f'voce e menor ds idade { idade }');
+
+
+# na empresao acima podemos ver que se o valor idade for maior ou ingual a 18 nao imprime nada na tela
+#podemos mudar isso com o else:
+idade2 = 19;
+
+if idade2 < 18:
+    print(f'voce e menor ds idade { idade2 }');
+else:
+    print(f'voce e maior de idade { idade2 }');
