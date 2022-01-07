c8aa5b7be022814bf7d755b1468df8913458a790
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-07 11:07:15 +0200

    update

--- src/programacao-orientada-objectos/primeira-class-em-python.py
+++ src/programacao-orientada-objectos/primeira-class-em-python.py
@@ -1,12 +1,12 @@
 class MinhaClasse:
-    def __init__(self, nome):
-        self.nome = nome;
+    def __init__(self, nome): # o __init__ e o contructor da class.
+        self.nome = nome; # o self representa a class MinhaClasse
 
     def acessarONome(self):
         print(self.nome)
 
 
-    def deletarONome(self):
+    def deletarONome(self):# e obrigatorio que passe o paramentro self em cada funcoa criada.
         self.nome = '';
         print(self.nome)
 
